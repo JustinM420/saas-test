ba1158f6ca2179584be5033e16c55c97de2ea4bf
Weedsociety <103718091+Weedsociety@users.noreply.github.com>
2023-10-30 20:44:47 -0700

    added openai and built pdf chat page

--- saas-test/src/app/chat/[chatId]/page.tsx
+++ saas-test/src/app/chat/[chatId]/page.tsx
@@ -1,3 +1,6 @@
+import ChatComponent from '@/components/ChatComponent';
+import ChatSideBar from '@/components/ChatSideBar';
+import PDFViewer from '@/components/PDFViewer';
 import { db } from '@/lib/db';
 import { chats } from '@/lib/db/schema';
 import { auth } from '@clerk/nextjs';
@@ -24,15 +27,23 @@ const ChatPage = async ({params: { chatId } }: Props) => {
         return redirect('/');
     }
 
+    const currentChat = _chats.find(chat => chat.id === parseInt(chatId));
+
     return(
         <div className="flex max-h-screen overflow-scroll"> 
             <div className="flex w-full max-h-screen overflow-scroll">
                 {/* chat sidebar */}
-                <div className="flex-[1] max-w-xs"></div>
+                <div className="flex-[1] max-w-xs">
+                    <ChatSideBar chats={_chats} chatId={parseInt(chatId)}/>
+                </div>
                 {/* pdf viewer */}
-                <div className="max-h-screen p-4 overflow-scroll flex-[5]"></div>
+                <div className="max-h-screen p-4 overflow-scroll flex-[5]">
+                    <PDFViewer pdf_url={currentChat?.pdfUrl || ''}/>
+                </div>
                 {/* chat component */}
-                <div className="flex-[3] border-1-4 border-1-slate-200"></div>
+                <div className="flex-[3] border-1-4 border-1-slate-200">
+                    <ChatComponent chatId={parseInt(chatId)}/>
+                </div>
             </div>
         </div>
     ) 
